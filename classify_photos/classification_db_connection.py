f7e49cb5c2725e8d5fd47d1fe405e2bf1c666a91
Matthew Carroll <matthew.j.carroll@outlook.com>
2019-04-01 19:00:33 -0500

    read in images from a directory

--- classify_photos/classification_db_connection.py
+++ classify_photos/classification_db_connection.py
@@ -1,1 +1,14 @@
 #this will connect the classifier to the dataset (likely in flask) and have premade queries written
+import os
+import cv2
+
+#read in a literal string path name
+path = input()
+#build a list of each of the contents of the path
+dir_content = os.listdir(path)
+
+img_list = [];
+#for each item in the list read in the image and store to img_list
+for name in dir_content:
+    img_list.append(cv2.imread(name))
+
