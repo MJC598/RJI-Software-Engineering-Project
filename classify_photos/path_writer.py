432b1d937e85bd9b758dd4bcbf8461b5dc1ad359
Matthew Carroll <matthew.j.carroll@outlook.com>
2019-04-11 13:37:58 -0500

    bug fix

--- classify_photos/path_writer.py
+++ classify_photos/path_writer.py
@@ -3,7 +3,7 @@ import os
 f = []
 file_list = []
 
-for(dirpath, _, filenames) in walk(mypath):
+for(dirpath, _, filenames) in os.walk(mypath):
     for f in filenames:
         file_list.append(os.path.abspath(os.path.join(dirpath, f)))
 
