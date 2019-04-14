f798aa0d2c87b1675a4e4bf2e217353f4079157e
Livvy <orb998@mail.missouri.edu>
2019-04-14 17:19:31 -0500

    added index.js

---
+++ ui/index.js
@@ -0,0 +1,13 @@
+function display_images() {
+
+	// function will display images read from tempory directory
+	
+}
+
+
+function display_metadata() {
+
+	// function will display corresponding metadata with each image
+	// will need to retrieve data from classifier via controller
+
+}
