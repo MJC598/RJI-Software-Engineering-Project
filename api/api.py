84e2108392a3ec35577fcfaadbc3c97b430a2cd3
EC2 Default User <ec2-user@ip-172-31-43-221.us-east-2.compute.internal>
2019-04-18 19:06:16 +0000

    updated api.py - function returns json string of images from url

--- api/api.py
+++ api/api.py
@@ -1,4 +1,4 @@
-import flask
+"""import flask
 from flask import request, jsonify
 
 app = flask.Flask(__name__)
@@ -31,4 +31,60 @@ def api_all():
     return jsonify(books)
 
 
-app.run()
+app.run()"""
+import requests, re, json
+
+# creates a json string of tuples in the form [('filename', 'full_url_of_image')]
+def get_images_from_url(url):
+    
+    # get the directory, its output is basically an HTML document with anchor tags for each image
+    r = requests.get(url)
+
+    # parses the HTML document and grabs everything inside quotation marks and stores these filenames in list a
+    a = re.findall('"([^"]*)"', r.text)
+
+    # add the url to each filename and store this full path in list b
+    url += '{0}'
+    b = [url.format(i) for i in a]
+
+    # zip the two lists together and ditch the first element since it's invariably '../'; store in list c
+    c = list(zip(a,b))
+    c.pop(0)
+
+    # turn c into a json_string
+    json_string = json.dumps(c)
+
+    # this is for testing. I do not recommend uncommenting this line, it is a very large output.
+    # print json_string
+
+    return json_string
+
+def get_sample_images_json():
+    json_string1 = get_images_from_url('http://rji.augurlabs.io/20170902_MuMsu/1q/')
+    json_string1 = json_string1[:-1] + ','
+    json_string2 = get_images_from_url('http://rji.augurlabs.io/20170902_MuMsu/2q/')
+    json_string2 = " " + json_string2[1:]
+    json_string2 = json_string2[:-1] + ','
+    json_string3 = get_images_from_url('http://rji.augurlabs.io/20170902_MuMsu/3q/')
+    json_string3 = " " + json_string3[1:]
+    json_string3 = json_string3[:-1] + ','
+    json_string4 = get_images_from_url('http://rji.augurlabs.io/20170902_MuMsu/4q/')
+    json_string4 = " " + json_string4[1:]
+    json_string4 = json_string4[:-1] + ',' 
+    json_string5 = get_images_from_url('http://rji.augurlabs.io/20170909_MuSc/1q/')
+    json_string5 = " " + json_string5[1:]
+    json_string5 = json_string5[:-1] + ',' 
+    json_string6 = get_images_from_url('http://rji.augurlabs.io/20170909_MuSc/2q/')
+    json_string6 = " " + json_string6[1:]
+    json_string6 = json_string6[:-1] + ','
+    json_string7 = get_images_from_url('http://rji.augurlabs.io/20170909_MuSc/3q/')
+    json_string7 = " " + json_string7[1:]
+    json_string7 = json_string7[:-1] + ',' 
+    json_string8 = get_images_from_url('http://rji.augurlabs.io/20170909_MuSc/4q/')
+    json_string8 = " " + json_string8[1:]
+    json_string = json_string1 + json_string2 + json_string3 + json_string4 + json_string5 + json_string6 + json_string6 + json_string7 + json_string8
+    with open('data.json', 'w') as outfile:
+        json.dump(json_string,outfile)
+
+
+get_sample_images_json()
