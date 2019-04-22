e5da4ed70938608ca1554d82661e9a607bec5004
EC2 Default User <ec2-user@ip-172-31-43-221.us-east-2.compute.internal>
2019-04-22 20:41:44 +0000

    put ui code in ui from dev environment

---
+++ ui/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head lang="en">
+		<meta charset="utf-8">
+		<title>RJI Classifier</title>
+		<link href="index.css" rel="stylesheet">
+	</head>	
+	<body>
+	    <ul>
+		<li><a class="active" href="">Search</a></li>
+		<li><a href="">About</a></li>
+		<li><a href="">Contact</a></li>
+	    </ul>
+	    <div id="container">
+		<h1>RJI Classifier</h1>
+            <!--<button type="button">View Image Score!</button> -->
+            
+		 <div id="search_div">
+		    <input id="search_bar" type="text" placeholder="Search..">
+		</div>  
+	    </div>
+	</body>
+</html>
