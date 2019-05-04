7e934e8ea398e0f8cd02e1ae00cc961793537532
Matthew Carroll <matthew.j.carroll@outlook.com>
2019-05-04 10:54:31 -0500

    undo it

--- classifier/run_classifier.sh
+++ classifier/run_classifier.sh
@@ -1,4 +1,4 @@
-source /var/www/image-quality-assessment/contrib/tf_serving/venv_tfs_nima/bin/activate \
-&& python3 -W ignore -m .var.www.image-quality-assessment.contrib.tf_serving.tfs_sample_client \
+source contrib/tf_serving/venv_tfs_nima/bin/activate \
+&& python3 -W ignore -m contrib.tf_serving.tfs_sample_client \
 --image-path=/var/www/RJI-Software-Engineering-Project/pictures/rji.augurlabs.io/20170902_MuMsu/1q/20170921_mumsu_mp_1399.JPG \
 --model-name=mobilenet_technical
